be9d2fc0b558cd4609414e968f80e25962960ac0
ItoRenz <ridwan.ri214@gmail.com>
2022-04-24 17:02:37 +0000

    santoni: nad bringup
    Signed-off-by: ItoRenz <itorenz@nusantararom.org>

--- nad_santoni.mk
+++ nad_santoni.mk
@@ -19,27 +19,24 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk)
 
+# Inherit some common Nusantara stuff.
+NAD_BUILD_TYPE := OFFICIAL
+USE_PIXEL_CHARGING := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+$(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
+
 # Inherit from santoni device
 $(call inherit-product, device/xiaomi/santoni/device.mk)
-
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
-# Prebuilt apps
-$(call inherit-product-if-exists, vendor/prebuilt-apps/config.mk)
-
-# GApps
-$(call inherit-product-if-exists, vendor/gapps/core/config.mk)
 
 PRODUCT_DEVICE := santoni
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 4X
-PRODUCT_NAME := lineage_santoni
+PRODUCT_NAME := nad_santoni
 BOARD_VENDOR := Xiaomi
 PRODUCT_MANUFACTURER := Xiaomi
 
 # Overlay
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-nad
 
 # Touch HAL
 PRODUCT_PACKAGES += \
@@ -55,7 +52,7 @@ PRODUCT_PACKAGES += \
 
 # Boot animation
 TARGET_BOOT_ANIMATION_RES := 720
-	
+
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
