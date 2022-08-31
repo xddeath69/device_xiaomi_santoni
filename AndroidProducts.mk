d8af6e5cb9484cde2aa0d017f46031da280fb99f
xddeath69 <85794863+xddeath69@users.noreply.github.com>
2022-08-31 13:21:26 -0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/nad_santoni.mk
+    $(LOCAL_DIR)/havoc_santoni.mk
 
 COMMON_LUNCH_CHOICES := \
-    nad_santoni-userdebug \
-    nad_santoni-user \
-    nad_santoni-eng
+    havoc_santoni-userdebug \
+    havoc_santoni-user \
+    havoc_santoni-eng
